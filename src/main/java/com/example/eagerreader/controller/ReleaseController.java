346fad180cfa30e7b726c9d18f625837d83aa2bc
kasiagrygorowicz <kasiagrygorowicz@interia.eu>
2022-04-29 16:25:23 +0200

    add and edit page for release

---
+++ src/main/java/com/example/eagerreader/controller/ReleaseController.java
@@ -0,0 +1,20 @@
+package com.example.eagerreader.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/release")
+public class ReleaseController {
+
+    @GetMapping("/add")
+    public String displayAddNewReleaseForm(){
+        return "releases/add-release";
+    }
+
+    @GetMapping("/edit")
+    public String displayEditReleaseForm(){
+        return "releases/edit-release";
+    }
+}
